d842a28a282f1f73bb67077b3aec1544e7543de9
travenupai <ai.travenup@gmail.com>
2024-12-08 13:29:13 -0300

    Fix: Ajuste de imports e dependências

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os
 import sys
 #__import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from src.swot.crew import SwotCrew
 import streamlit as st
 from io import BytesIO
